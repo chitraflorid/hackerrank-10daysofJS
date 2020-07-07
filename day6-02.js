1f61d876287a15dd80da47646667563285e2f226
chitra <chitraflorid@users.noreply.github.com>
2020-07-07 11:52:44 +0530

    Create day6-02.js

---
+++ day6-02.js
@@ -0,0 +1,14 @@
+
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    const daysList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const userDate = new Date(dateString);
+
+    dayName = daysList[userDate.getDay()];
+    
+    return dayName;
+}
+
