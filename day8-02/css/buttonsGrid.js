485509e72d6670ad284fcf3bc6270b907345160b
chitra <chitraflorid@users.noreply.github.com>
2020-07-07 11:59:35 +0530

    Create buttonsGrid.js

---
+++ day8-02/css/buttonsGrid.js
@@ -0,0 +1,31 @@
+body {
+    margin: 0;
+    padding: 0px;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+#btns {
+    position: relative;
+    width: 75%;
+    display: grid;
+    grid-template-columns: 25% 25% 25%;
+    justify-content: center;
+    align-content: center;
+}
+
+#btns > div {
+    font-size: 24px;
+    border: 1px solid gray;
+    background-color: #e0e7ea;
+    height: 48px;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 48px;
+}
+
+#btn5 {
+    cursor: pointer;
+}
